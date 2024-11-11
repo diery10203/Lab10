f6c8de2e77bced7a2b3ff63fa9f3ca15c57ee520
diery10203 <huuduy2003@gmail.com>
2024-11-11 13:24:28 +0700

    Fix App.js

--- Bike_Redux/App.js
+++ Bike_Redux/App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import Screen01 from './src/pages/Screen01';
+import Screen02 from './src/pages/Screen02';
+import Admin from './src/pages/Admin';
+import EditBike from './src/pages/EditBike';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Provider } from 'react-redux';
+import { store } from './src/redux/store';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Screen01" component={Screen01} options={{ headerShown: false }} />
+          <Stack.Screen name="Screen02" component={Screen02} options={{ headerShown: false }} />
+          <Stack.Screen name="Admin" component={Admin} options={{ headerShown: false }} />
+          <Stack.Screen name="EditBike" component={EditBike} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider> 
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
